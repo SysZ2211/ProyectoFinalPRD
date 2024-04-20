176c8aa970866903f8ed2f40b9cbd6cdf868d4b5
Mega <Mega@DESKTOP-3LO51PB>
2024-04-20 10:50:17 -0500

    Comienzo Clases quemadas

--- proyectoFinal/src/co/edu/UEF/asignaturasOptc/vista/Principal.java
+++ proyectoFinal/src/co/edu/UEF/asignaturasOptc/vista/Principal.java
@@ -15,17 +15,31 @@ import co.edu.UEF.asignaturasOptc.servicios.impleCRUD;
 public class Principal {
     
     public static void main(String[] args) {
-
+  //CLASES QUEMADAS O DISPONIBLES
     	int[][] arr = new int[][]{{5,1},{2,3},{2,4}};
-
-    	
-    	
     	Clase Paradigmas = new Clase("Paradigmas de programacion", "UEF3718",5, arr);
+    	
+    	
+    	
+    	int[][] arr1 = new int[][]{{0,0},{1,0},{2,0}};
+    	Clase Estructuras = new Clase("Estructuras de datos", "HJD6711",7, arr1);
+    	
+    	
+    	int[][] arr3 = new int[][]{{5,1},{4,3},{5,2}};
+    	Clase Create = new Clase("CreateCamps", "FGT2614",10, arr3);
+    	/*
+    	int[][] arr3 = new int[][]{{5,1},{2,3},{2,4}};
+    	Clase Paradigmas = new Clase("Paradigmas de programacion", "UEF3718",5, arr);
+    	int[][] arr4 = new int[][]{{5,1},{2,3},{2,4}};
+    	Clase Paradigmas = new Clase("Paradigmas de programacion", "UEF3718",5, arr);
+    	**/
+    	
     
+    	String rojo = "\033[31m";
     	
     	
     	
-    	Clase[] clases1 = {Paradigmas};
+    	Clase[] clases1 = {Paradigmas,Estructuras};
     	
     	
     	Carrera Ing_Sistemas = new Carrera("Ingenieria de Sistemas", 8, 150, clases1);
@@ -35,7 +49,7 @@ public class Principal {
     	impleCRUD Hunal = new impleCRUD();
     //PARA EDITAR EL MENU (ESTO ESTA PENDIENTE), HAY QUE EDITAR LAS OPCIONES EN EL STRING + IMPLEMENTAER LOS METODOS DENTRO DEL MENU
     	
-System.out.println("Elija una opción:"+"\n"+ "1. Pedir Objeto"+"\n"+"2. Read"+"\n"+"3. Update"+"\n"+"4. Delete"+"\n"+"5. Guardar Archivo"+"\n"+"6. Cargar Archivo"+"\n"+"7. Salir");
+System.out.println("Elija una opción:"+"\n"+ "1. Asignar Clase"+"\n"+"2. Read"+"\n"+"3. Update"+"\n"+"4. Delete"+"\n"+"5. Guardar Archivo"+"\n"+"6. Cargar Archivo"+"\n"+"7. Salir");
     	
     	Scanner in = new Scanner(System.in);
     	int opcion;
@@ -46,7 +60,15 @@ System.out.println("Elija una opción:"+"\n"+ "1. Pedir Objeto"+"\n"+"2. Read"+"
     		if (opcion == 1) {//CREATE
     			
     			
+    			
+    			
+    			
+    			
+    			
     			System.out.println(Hunal.Create(Paradigmas));
+
+    			System.out.println(Hunal.Create(Estructuras));
+    			
     			
     		}
     		if (opcion == 2) {//READ ALL
